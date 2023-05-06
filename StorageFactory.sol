ac826556c091d2ba680a09668639c33fb9c2bb52
JARAYTIDA <rajadityakumawat@gmail.com>
2023-05-06 07:28:04 +0530

    interacting with contracts

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -11,4 +11,12 @@ contract StorageFactory{
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
+
+    function sfStore(uint256 _index, uint256 _favNumber) public {
+        simpleStorageArray[_index].store(_favNumber);
+    }
+
+    function sfGet(uint256 _index) public view returns(uint256){
+        return simpleStorageArray[_index].retrieve();
+    }
 }
